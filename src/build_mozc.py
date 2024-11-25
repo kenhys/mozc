9d74afebc16ac4bde46573e346540e8af33b7543
Kentaro Hayashi <kenhys@gmail.com>
2024-11-25 22:03:58 +0900

    Revert "Remove the Ibus build from build_mozc.py."
    Description: enable --use_gyp_for_ibus_build again
    Origin: https://github.com/google/mozc/commit/59cdfc191159a2e6d1e9a344e7fb8868c21c09cb
    Forwarded: not-needed
    Last-Update: 2024-11-25
    Signed-off-by: Kentaro Hayashi <kenhys@gmail.com>

--- src/build_mozc.py
+++ src/build_mozc.py
@@ -76,8 +76,14 @@ ABS_SCRIPT_DIR = CaseAwareAbsPath(os.path.dirname(__file__))
 MOZC_ROOT = ABS_SCRIPT_DIR
 EXT_THIRD_PARTY_DIR = os.path.join(MOZC_ROOT, 'third_party')
 
-sys.path.append(SRC_DIR)
+# Ibus build is no longer supported by GYP build.
+# The build rules and code will be removed in future.
+#   https://github.com/google/mozc/issues/567
+# Bazel build is the alternative.
+#   https://github.com/google/mozc/blob/master/docs/build_mozc_in_docker.md
+USE_UNSUPPORTED_IBUS_BUILD = True
 
+sys.path.append(SRC_DIR)
 
 def GetMozcVersion():
   """Returns MozcVersion instance."""
@@ -163,7 +169,10 @@ def GetGypFileNames(options):
   if options.target_platform == 'Windows':
     gyp_file_names.extend(glob.glob('%s/win32/*/*.gyp' % OSS_SRC_DIR))
   elif options.target_platform == 'Linux':
-    gyp_file_names.extend(glob.glob('%s/unix/emacs/*.gyp' % OSS_SRC_DIR))
+    gyp_file_names.extend(glob.glob('%s/unix/*/*.gyp' % OSS_SRC_DIR))
+    # Add ibus.gyp if ibus version is >=1.4.1.
+    if not USE_UNSUPPORTED_IBUS_BUILD:
+      gyp_file_names.remove('%s/unix/ibus/ibus.gyp' % OSS_SRC_DIR)
   gyp_file_names.sort()
   return gyp_file_names
 
@@ -292,6 +301,10 @@ def ExpandMetaTarget(options, meta_target_name):
         OSS_SRC_DIR + '/server/server.gyp:mozc_server',
         OSS_SRC_DIR + '/gui/gui.gyp:mozc_tool',
     ]
+    if USE_UNSUPPORTED_IBUS_BUILD:
+      # GYP no longer support Ibus builds.
+      # USE_UNSUPPORTED_IBUS_BUILD should be False unless the code is modified.
+      targets.append(OSS_SRC_DIR + '/unix/ibus/ibus.gyp:ibus_mozc')
   elif target_platform == 'Mac':
     targets = [OSS_SRC_DIR + '/mac/mac.gyp:codesign_DiskImage']
   elif target_platform == 'Windows':
@@ -313,6 +326,7 @@ def ParseBuildOptions(args):
   AddCommonOptions(parser)
   parser.add_option('--configuration', '-c', dest='configuration',
                     default='Debug', help='specify the build configuration.')
+  parser.add_option('--use_gyp_for_ibus_build', action='store_true')
 
   (options, args) = parser.parse_args(args)
 
@@ -320,8 +334,33 @@ def ParseBuildOptions(args):
   for arg in args:
     targets.extend(ExpandMetaTarget(options, arg))
 
+  CheckIbusBuild(options, targets)
   return (options, targets)
 
+def CheckIbusBuild(options, targets):
+  """Check if targets contains ibus builds without the command flag."""
+  if options.use_gyp_for_ibus_build:
+    return
+
+  has_ibus_build = False
+  for target in targets:
+    if 'ibus' in target:
+      has_ibus_build = True
+      break
+  if not has_ibus_build:
+     return
+
+  message = [
+      'The GYP build will stop supporting IBus client and renderer.',
+      'https://github.com/google/mozc/issues/567',
+      '',
+      'The Bazel build is the alternative.',
+      'https://github.com/google/mozc/blob/master/docs/build_mozc_in_docker.md',
+      '',
+      'To keep using the GYP build at this moment,',
+      'please add the --use_gyp_for_ibus_build flag to build_mozc.py.',
+  ]
+  PrintErrorAndExit('\n'.join(message))
 
 def ParseRunTestsOptions(args):
   """Parses command line options for the runtests command."""
